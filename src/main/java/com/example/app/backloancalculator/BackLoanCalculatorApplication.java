a97683f5c225fcec8e55a747af345325299f5f25
Vlad58R <vadja1612@gmail.com>
2023-02-13 00:26:46 +0200

    adding initial spring boot files

---
+++ src/main/java/com/example/app/backloancalculator/BackLoanCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.app.backloancalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackLoanCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackLoanCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/app/backloancalculator/BackLoanCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app.backloancalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackLoanCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
